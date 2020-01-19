9686b05229ad8af6166616e76056597fd2d8d0f4
hoya-1004 <52414893+hoya-1004@users.noreply.github.com>
2020-01-19 23:32:28 +0900

    review

---
+++ test.js
@@ -0,0 +1,102 @@
+/* String
+const what = "Ho";
+*/
+/*
+Boolean
+const wat = ture;
+*/
+/*
+Number
+const wat = 666;
+*/
+
+/*
+Float(double)
+const wat = 55.1;
+*/
+
+//console.log('Im Working. Im JS. Im Beautiful. Im worth it');
+
+//Array
+/*const daysOfWeek = ["Mon","Tue","Wen","Thu","Fri",true];
+
+console.log(daysOfWeek[2]);
+*/
+/*
+const hoInfo = {
+    name:"Ho",
+    age:33,
+    gender:"Male",
+    isHandsome :true,
+    favMovies :["About Time", "LOTR"],
+    favFood : [
+        {  
+            name : "Kimchi",
+            fatty : false
+        },
+        {
+            name : "CheeseBurger",
+            fatty : true
+        }
+    ]
+}
+console.log(hoInfo);
+*/
+/*
+function sayHello(){
+    console.log('Hello!');
+}
+
+sayHello();
+*/
+/*
+function sayHello(name, age){
+    console.log('Hello!',name, "You have", age,"years fo age.");
+}
+
+sayHello("HO",26);
+*/
+
+/*
+function sayHello(name, age){
+    console.log(`Hello! ${name} You have ${age} years fo age.`);
+}
+
+sayHello("HO",26);
+*/
+/*
+function sayHello(name, age){
+    return `Hello! ${name} You have ${age} years fo age.`;
+}
+
+const greetHo = sayHello("Ho", 26)
+
+console.log(greetHo)
+*/
+/*
+const calculator ={
+plus : function(a,b){
+    return a +b;
+}
+}
+//console.log(greetHo)
+const plus = calculator.plus(5,5)
+console.log(plus)
+*/
+/*
+const title = document.querySelector("#title");
+title.innerHTML = "Hi! From JS";// HTML을 DOM으로 
+title.style.color = 'red';
+
+document.title = "I own you now!";
+*/
+/*
+const title = document.querySelector("#title");
+
+function handleResize(event){
+    //console.log("I have been resized")
+    console.log(event);
+}
+
+window.addEventListener("resize", handleResize); //handleResize()를 쓴다면 즉시 바꾼다. 그게 아니고 지금 처럼 되어있는 경우는 즉시 호출하지 않고 윈도우 사이즈가 변경될때 함수를 호출한다.
+*/
