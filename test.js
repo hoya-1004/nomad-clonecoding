1e0eb6284ca9846a471b3d4e7a53b717c8875cf9
hoya-1004 <52414893+hoya-1004@users.noreply.github.com>
2020-01-20 23:19:56 +0900

    modification

--- test.js
+++ test.js
@@ -100,3 +100,79 @@ function handleResize(event){
 
 window.addEventListener("resize", handleResize); //handleResize()를 쓴다면 즉시 바꾼다. 그게 아니고 지금 처럼 되어있는 경우는 즉시 호출하지 않고 윈도우 사이즈가 변경될때 함수를 호출한다.
 */
+/*
+const title = document.querySelector("#title");
+
+function handleClick(){
+    title.style.color = "blue";
+}
+title.addEventListener("click", handleClick);
+*/
+/*
+if("10" === 10){ //===는 값을 할당하는게 아니라 just 체크
+    console.log("HI");
+}else if ("10"==="10"){
+    console.log("lalala");
+}else{
+    console.log("ho");
+} */
+/*
+if(20 > 5 ||"h" === "ho")
+{
+    console.log("yes");
+}
+else{
+    console.log("no");
+} 
+|| && 기타 연습
+ */
+
+/*
+const age = prompt("How old are you");
+
+if(age >= 18 && age <=21){
+    console.log("you can drink but you should not");
+}else if(age >21){
+    console.log("go ahead");
+}
+else{
+    console.log("too young");
+}
+*/
+
+/*const title = document.querySelector("#title");
+
+//색깔이 같은지 확인한다음 색깔을 바꾼다.
+const BASE_COLOR = "rgb(52, 73, 94)";
+const OTHER_COLOR = "#7f8c8d";
+
+
+function handClick(){
+    //console.log(title.style.color); 
+    const currentColor = title.style.color;
+    if(currentColor === BASE_COLOR){
+        title.style.color = OTHER_COLOR;
+    }
+    else{
+        title.style.color = BASE_COLOR; 
+    }
+}
+
+function init(){
+    title.style.color = BASE_COLOR;
+    title.addEventListener("click", handClick);
+}
+init();
+*/
+/* 인터넷 연결이 연결되거나 끊겼을때 console
+function handleOffline() {
+    console.log("byeybye");
+}
+
+function handleOnline() {
+    console.log("Welcome back");
+}
+window.addEventListener("offline", handleOffline);
+window.addEventListener("online", handleOnline);
+
+*/
