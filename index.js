368a14e985e0090c904bb3cc24701c58a950a89a
hoya-1004 <52414893+hoya-1004@users.noreply.github.com>
2020-01-21 23:43:51 +0900

    modification

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log('Im Working. Im JS. Im Beautiful. Im worth it');
+const title = document.querySelector("#title");
+
+const CLICKED_CLASS = "clicked";
+
+
+//toggle은 했던 모든걸 요약하는것
+function handleClick() {
+    title.classList.toggle(CLICKED_CLASS);
+}
+
+function init() { 
+    title.addEventListener("click", handleClick);
+}
+init();
