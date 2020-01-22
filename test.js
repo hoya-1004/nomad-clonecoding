ed36b894266fab547e1f31c049cca42e87bce926
hoya-1004 <52414893+hoya-1004@users.noreply.github.com>
2020-01-22 21:32:58 +0900

    modification

--- test.js
+++ test.js
@@ -234,3 +234,20 @@ function init() {
 }
 init();
 */
+
+/*
+const title = document.querySelector("#title");
+
+const CLICKED_CLASS = "clicked";
+
+
+//toggle은 했던 모든걸 요약하는것
+function handleClick() {
+    title.classList.toggle(CLICKED_CLASS);
+}
+
+function init() { 
+    title.addEventListener("click", handleClick);
+}
+init();
+*/
