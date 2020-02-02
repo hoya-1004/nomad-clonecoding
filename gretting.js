37660cd266a54b2d2b13a6c04f0c747071d04a09
hoya-1004 <52414893+hoya-1004@users.noreply.github.com>
2020-02-02 23:05:33 +0900

    modification

--- gretting.js
+++ gretting.js
@@ -5,22 +5,6 @@
 const USER_LS = "currentUser",
  SHOWING_CN = "showing";
 
-function saveName(text){
-    localStorage.setItem(USER_LS,text);
-}
-
-function handleSubmit(event){
-    event.preventDefault();
-    const currentValue = input.value;
-    paintGreeting(currentValue);
-    saveName(currentValue);
-}
-
-function askForName(){
-    form.classList.add(SHOWING_CN);
-    form.addEventListener("submit", handleSubmit)
-}
-
  function paintGreeting(text) {
      form.classList.remove(SHOWING_CN);
      greeting.classList.add(SHOWING_CN);
@@ -30,7 +14,6 @@ function askForName(){
  function loadName(){
      const currentUser = localStorage.getItem(USER_LS);
      if(currentUser === null){
-         askForName();
      }
      else{
          paintGreeting(currentUser);
