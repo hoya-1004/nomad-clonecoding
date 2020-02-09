f37425654292d545036032b4cf90082899730fa4
hoya-1004 <52414893+hoya-1004@users.noreply.github.com>
2020-02-09 22:33:34 +0900

    modification

--- todo.js
+++ todo.js
@@ -1,12 +1,12 @@
-const toDoForm = document.queryCommandEnabled(".js-toDoForm"),
+const toDoForm = document.querySelector(".js-toDoForm"),
  toDoInput = toDoForm.querySelector("input"),
- toDoList = document.querySelector("js-toDoList");
+ toDoList = document.querySelector(".js-toDoList");
 
 const TODOS_LS = 'toDos';
 
 const toDos = [];
 
-function deleteToDO(event){
+function deleteToDo(event){
     const btn = event.target;
     const li = btn.parentNode;
     toDoList.removeChild(li);
@@ -45,8 +45,8 @@ function handleSubmit(event){
     toDoInput.value = "";
 }
  function loadToDos(){
-     const loadToDos = localStorage.getItem(TODOS_LS);
-     if(loadToDos !== null){
+     const toDos = localStorage.getItem(TODOS_LS);
+     if(toDos !== null){
         const parsedToDos = JSON.parse(loadedToDos);
         parsedToDos.forEach(function(toDo){
             paintToDo(toDo.text);
